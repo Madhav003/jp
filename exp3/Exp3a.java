957861eef3f512ce5347dc314b883b0476bc1782
Madhav003 <madhavbhalodia2006@gmail.com>
2026-01-07 16:08:17 +0530

    Reorganize project structure

---
+++ exp3/Exp3a.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class Exp3a{
+  public static void main(String args[]){
+    int ans = 0;
+    Scanner sc = new Scanner(System.in);
+    while(true){
+    System.out.println("Basic Calculator");
+    System.out.print("Enter number 1: ");
+    int a = sc.nextInt();
+    System.out.print("\nEnter number 2: ");
+    int b = sc.nextInt();
+    System.out.print("Enter operation(+,-,*,/,<<,>>,&,|): ");
+    char c = sc.next().charAt(0);
+    switch(c) {
+    case '+':
+        ans = a+b;
+        break;
+    case '-':
+        ans = a-b;
+        break;
+    case '*':
+        ans = a*b;
+        break;
+    case '/':
+        ans = a/b;
+        break;
+    case '<<':
+        ans = a << b;
+        break;
+    case '>>':
+        ans = a >> b;
+        break;
+    case '&':
+        ans = a & b;
+        break;
+    case '|':
+        ans = a | b;
+        break;
+    case '~':
+        ans = ~a;
+        break;
+    default:
+        System.out.println("Invalid Input");
+    }
+    System.out.println("Ans: " + ans);
+    }
+  }
+}
---
+++ exp3/Exp3b.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Exp3b{
+  public static void main(String args[]){
+    int arr[] = new int[5];
+    Scanner sc = new Scanner(System.in);
+    for(int i = 0; i<5; i++){
+    arr[i] = sc.nextInt();
+    }
+    int sum = 0;
+    boolean found = false;
+    for(int i = 0; i<5; i++){
+      sum = 0;
+      for(int j=i;j<5;j++){
+        System.out.print(arr[j]);
+        sum = sum + arr[j];
+        if(sum == 0){
+          found = true;
+          break;
+        }
+      }
+      System.out.println("");
+    }
+    if(found == true) System.out.println("Yes");
+    if(found == false) System.out.println("No");
+  }
+}
